813f3fecf58eb4b26e7242e8b1b02ff9e6bb09c8
Dmitry Kisler <admin@dkisler.de>
2018-01-06 23:34:34 +0100

    modified the code

--- R/gadPlot.R
+++ R/gadPlot.R
@@ -1,17 +1,15 @@
 gadPlot <- function(country, type = "country")
 {
-  if( length( grep("country|json", type) ) == 0 ) {
+  if( length( grep("country|json", type) ) == 0 ) 
     return(NA)
-  } else {
-    m <- leaflet() %>% addProviderTiles("Esri.WorldTopoMap")
-    if(type == "country")
-    {
-      map <- getGeoJson(country, iso = F, lvl = 0, saveRaw = F, saveJson = F)
-      m <- m %>% addGeoJSON(map$json) %>% fitBounds(lng1 = map$bbox$lon_min, lng2 = map$bbox$lon_max,
-                                                    lat1 = map$bbox$lat_min, lat2 = map$bbox$lat_max)
-    }
-    if(type == "json")
-      m <- m %>% addGeoJSON(country)
-    return( m )
+  m <- leaflet() %>% addProviderTiles("Esri.WorldTopoMap")
+  if(type == "country")
+  {
+    map <- getGeoJson(country, iso = F, lvl = 0, saveRaw = F, saveJson = F)
+    m <- m %>% addGeoJSON(map$json) %>% fitBounds(lng1 = map$bbox$lon_min, lng2 = map$bbox$lon_max,
+                                                   lat1 = map$bbox$lat_min, lat2 = map$bbox$lat_max)
   }
+  if(type == "json")
+    m <- m %>% addGeoJSON(country)
+  return( m )
 }
